ca45da9d906892860d182bd25a262ba94c5e2b91
Anders Magnusson <anders14magnusson@gmail.com>
2020-12-13 13:26:06 +0100

    Install discord plugin

--- src/main/kotlin/com/github/gimme/omidbot/OmidBot.kt
+++ src/main/kotlin/com/github/gimme/omidbot/OmidBot.kt
@@ -1,10 +1,12 @@
 package com.github.gimme.omidbot
 
 import com.github.gimme.gimmebot.core.GimmeBot
+import com.github.gimme.gimmebot.discord.DiscordPlugin
 import com.github.gimme.omidbot.commands.MortgageCommand
 
 class OmidBot : GimmeBot() {
     override fun onStart() {
         commandManager.registerCommand(MortgageCommand())
+        install(DiscordPlugin())
     }
 }
